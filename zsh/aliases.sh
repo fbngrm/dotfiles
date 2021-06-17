1c5f2a730d973b0ba15d6fc6b8d8ffec19a5ae41
Fabian <fabian@pento.io>
2021-06-17 18:14:08 +0200

    add git config

--- zsh/aliases.sh
+++ zsh/aliases.sh
@@ -71,6 +71,7 @@ alias vt='nvim /tmp/t'
 alias ll='ls -alF'
 alias la='ls -A'
 alias l='ls -CF'
+alias cr='cp -R'
 # alias for long running commands.  Use like so:
 alias alert='notify-send --urgency=low -i "$([ $? = 0 ] && echo terminal || echo error)" "$(history|tail -n1|sed -e '\''s/^\s*[0-9]\+\s*//;s/[;&|]\s*alert$//'\'')"'
 
