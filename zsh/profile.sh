76a5409586e216a95d8bdb5cbdfe40860753608a
Fabian <fabian@pento.io>
2021-06-17 16:24:41 +0200

    add profile

---
+++ zsh/profile.sh
@@ -0,0 +1,14 @@
+# Nix shell
+# if [ -e /home/f/.nix-profile/etc/profile.d/nix.sh ]; then
+#   source /home/f/.nix-profile/etc/profile.d/nix.sh
+# fi
+
+# Sway
+systemctl --user import-environment
+systemctl --user start sway-session.target
+exec systemctl --wait --user start sway.service
+
+# if [ -z $DISPLAY ] && [ "$(tty)" == "/dev/tty1" ]; then
+  # exec "systemctl --user import-environment; systemctl --user start sway-session.target"
+  # exec sway
+# fi
